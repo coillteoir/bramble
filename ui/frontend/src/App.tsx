35f3c8bd857b338082a739d8191b37756dc1dd19
David <davite3@gmail.com>
2023-11-08 21:32:19 +0000

    tidied up for build

--- ui/frontend/src/App.tsx
+++ ui/frontend/src/App.tsx
@@ -1,17 +1,15 @@
 import { createSignal } from 'solid-js'
-import solidLogo from './assets/solid.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 
 
 function App() {
-    const [data, setData] = createSignal<string[] | null>(["Press button to get pods"])
+    const [data, setData] = createSignal<string[]>(["Press button to get pods"])
     const fetchData = async () => {
         try {
-            const response = await fetch('http://localhost:5555/')
-                            .then(response => response.json())
-                            .then(jsonData => setData(jsonData))
+            await fetch('http://localhost:5555/')
+                 .then(response => response.json())
+                 .then(jsonData => setData(jsonData))
         } catch (error) {
             console.error(error)
         }
