f4d862a9914ec070de20dd993946e632dba84d62
David <davite3@gmail.com>
2023-11-09 01:07:23 +0000

    minor improvements

--- ui/frontend/src/App.tsx
+++ ui/frontend/src/App.tsx
@@ -4,7 +4,7 @@ import './App.css'
 
 
 function App() {
-    const [data, setData] = createSignal<string[]>(["Press button to get pods"])
+    const [data, setData] = createSignal<{name: string, image: string}[]>([{name: "goobert", image: "snoobert"}])
     const fetchData = async () => {
         try {
             await fetch('http://localhost:5555/')
@@ -17,9 +17,10 @@ function App() {
 
   return (
     <>
-    <ul>
-    {data().map(e => <li>{e}</li>)}
-    </ul>
+    {data().map(e => 
+    <div class="pod">
+        <p>{(`Name:${e.name} Image:${e.image}`)}</p>
+    </div>)}
     <button onclick={fetchData}>Get data</button>
     </>
   )
