a4019c0d234035577636b0d278774300d1cda794
David <davite3@gmail.com>
2023-11-15 09:59:35 +0000

    using express generator

--- ui/backend/src/index.ts
+++ ui/backend/src/index.ts
@@ -1,38 +1,13 @@
-import express, { Express, Request, Response } from 'express'
-import cors from 'cors'
+import './pre-start'; // Must be the first import
+import logger from 'jet-logger';
 
-const k8s = require('@kubernetes/client-node');
+import EnvVars from '@src/constants/EnvVars';
+import server from './server';
 
-const kc = new k8s.KubeConfig();
-kc.loadFromDefault();
 
-const k8sApi = kc.makeApiClient(k8s.CoreV1Api);
+// **** Run **** //
 
-const getPo = async (ns: string) => {
-       const response =  await k8sApi.listNamespacedPod(ns)
-       const podNames = response.body.items.map(pod => ({
-           name: pod.metadata.name,
-           image: pod.spec.containers[0].image
-        }))
-       return podNames
-};
+const SERVER_START_MSG = ('Express server started on port: ' + 
+  EnvVars.Port.toString());
 
-const app: Express = express()
-
-const port: number = 5555
-
-app.use(cors())
-app.use(express.json())
-
-app.get('/', async (req: Request, res: Response) => {
-    try {
-        const podNames = await getPo('default')
-        res.json(podNames)
-    } catch (error) {
-        res.status(500).json({error: "internal server error (womp womp)"})
-    }
-})
-
-app.listen(port, () => {
-    console.log("Server is running")
-})
+server.listen(EnvVars.Port, () => logger.info(SERVER_START_MSG));
