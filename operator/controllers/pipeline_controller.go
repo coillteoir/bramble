85a710dafb7bac89d5dd54659bc8a6ff4b4939f2
David <davite3@gmail.com>
2023-12-04 13:20:07 +0000

    Task reconciler erroring out due to order of tasks in pipeline, not ideal

--- operator/controllers/pipeline_controller.go
+++ operator/controllers/pipeline_controller.go
@@ -18,13 +18,12 @@ package controllers
 
 import (
 	"context"
+	"fmt"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/log"
-	//	"time"
-	"fmt"
 
 	pipelinesv1alpha1 "github.com/davidlynch-sd/bramble/api/v1alpha1"
 )
@@ -39,15 +38,6 @@ type PipelineReconciler struct {
 //+kubebuilder:rbac:groups=pipelines.bramble.dev,resources=pipelines/status,verbs=get;update;patch
 //+kubebuilder:rbac:groups=pipelines.bramble.dev,resources=pipelines/finalizers,verbs=update
 
-// Reconcile is part of the main kubernetes reconciliation loop which aims to
-// move the current state of the cluster closer to the desired state.
-// TODO(user): Modify the Reconcile function to compare the state specified by
-// the Pipeline object against the actual cluster state, and then
-// perform operations to make the cluster state reflect the state specified by
-// the user.
-//
-// For more details, check Reconcile and its Result here:
-// - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.14.1/pkg/reconcile
 func (r *PipelineReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
 	_ = log.FromContext(ctx)
 
