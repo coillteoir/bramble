df6a9515345dae8ffcdf8787e367e084f9cfbcb1
David Lynch <davite3@protonmail.com>
2024-02-22 13:22:49 +0000

    Attempted to fix UI bug but it's still not working. Going to work through git proxy instead

--- operator/tests/e2e/e2e.sh
+++ operator/tests/e2e/e2e.sh
@@ -1,26 +1,20 @@
 #!/bin/sh
 
-
-which kind
-which make
-which go
-which kubectl
-which docker
-
 set -e
 
-kind create cluster --config=tests/e2e/test-cluster.yml
+FAIL=0
 
-make deploy
-
-echo "RUNNING TEST: SIMPLE PIPELINE"
-
-kubectl apply -f tests/e2e/resources/simple_pipeline.yaml
+kubectl apply -f tests/e2e/resources/one-to-many-execution.yaml
+kubectl apply -f tests/e2e/resources/one-to-many-pipeline.yaml
 kubectl apply -f tests/e2e/resources/simple_execution.yaml
+kubectl apply -f tests/e2e/resources/simple_pipeline.yaml
+kubectl apply -f tests/e2e/resources/sleepy_execution.yaml
+kubectl apply -f tests/e2e/resources/sleepy_pipeline.yaml
 
 echo "=======SLEEPING FOR 60 SECONDS======="
 sleep 60
 
+echo "RUNNING TEST: SIMPLE PIPELINE"
 
 SIMPLE_PODS=$(kubectl get po \
 -l=bramble-execution=simple-test \
@@ -30,20 +24,11 @@ echo "$SIMPLE_PODS"
 
 if [ $(echo "$SIMPLE_PODS" | wc -w) -eq '2' ] 
     then echo "Correct amount of pods have been created." 
-    else echo "Incorrect amount of pods created." && kind delete cluster --name=bramble-test-cluster && false
+    else echo "Incorrect amount of pods created." && FAIL=1
 fi
 
-kubectl delete pipeline simple
-kubectl delete execution simple
 
 echo "RUNNING TEST: SLEEPY PIPELINE"
-
-kubectl apply -f tests/e2e/resources/sleepy_pipeline.yaml
-kubectl apply -f tests/e2e/resources/sleepy_execution.yaml
-
-echo "=======SLEEPING FOR 60 SECONDS======="
-sleep 60
-
 
 SLEEPY_PODS=$(kubectl get po \
 -l=bramble-execution=sleepytest \
@@ -53,10 +38,27 @@ echo "$SLEEPY_PODS"
 
 if [ $(echo "$SLEEPY_PODS" | wc -w) -eq '7' ] 
     then echo "Correct amount of pods have been created." 
-    else echo "Incorrect amount of pods created." && kind delete cluster --name=bramble-sleepy-cluster && false
+    else echo "Incorrect amount of pods created." && FAIL=1
+fi
+
+echo "RUNNING TEST: ONE-TO-MANY PIPELINE"
+
+ONE_TO_MANY_PODS=$(kubectl get po \
+-l=bramble-execution=one-to-many \
+-o jsonpath='{.items[*].metadata.name}')
+
+echo "$ONE_TO_MANY_PODS"
+
+if [ $(echo "$ONE_TO_MANY_PODS" | wc -w) -eq '6' ] 
+    then echo "Correct amount of pods have been created." 
+    else echo "Incorrect amount of pods created." && FAIL=1
 fi
 
 kubectl delete pipeline sleepy
 kubectl delete execution sleepytest
+kubectl delete pipeline simple-test
+kubectl delete execution simple-test
+kubectl delete pipeline one-to-many
+kubectl delete execution one-to-many
 
-kind delete cluster --name=bramble-test-cluster
+exit $FAIL
