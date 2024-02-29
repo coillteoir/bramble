1722c98628576eb92a411874dcbd9971de39e192
David Lynch <davite3@protonmail.com>
2024-02-29 09:13:31 +0000

    Loading config for git proxy, not filling struct properly but good start

--- git-proxy/cmd/root.go
+++ git-proxy/cmd/root.go
@@ -14,11 +14,26 @@ import (
 	"github.com/google/go-github/v59/github"
 	"github.com/spf13/cobra"
 	"go.uber.org/zap"
+	"gopkg.in/yaml.v3"
 )
 
 const (
 	defaultPort = 9999
+	defaultPath = "config/samples/test_repo.yaml"
+	headPrefix  = "refs/heads/"
 )
+
+type proxyConfig struct {
+	GitRepo struct {
+		Provider string
+		Owner    string
+		Repo     string
+	}
+	BranchRules []struct {
+		Pattern  string
+		Pipeline string
+	}
+}
 
 var rootCmd = &cobra.Command{
 	Use:   "bramble-git-proxy",
@@ -36,6 +51,25 @@ var rootCmd = &cobra.Command{
 			return err
 		}
 
+		path, err := cmd.Flags().GetString("config")
+		if err != nil {
+			return err
+		}
+
+		configData, err := os.ReadFile(path)
+		if err != nil {
+			return err
+		}
+
+		config := []proxyConfig{}
+		err = yaml.Unmarshal(configData, &config)
+		if err != nil {
+			return err
+		}
+
+		fmt.Println(string(configData))
+		fmt.Println(config)
+
 		http.HandleFunc("/webhook", func(writer http.ResponseWriter, request *http.Request) {
 			payload, err := github.ValidatePayload(request, nil)
 			if err != nil {
@@ -52,7 +86,7 @@ var rootCmd = &cobra.Command{
 			switch event := event.(type) {
 			case *github.PushEvent:
 				sugar.Infof("Pushed!! %v", *event.Ref)
-				branchname, found := strings.CutPrefix(*event.Ref, "refs/heads/")
+				branchname, found := strings.CutPrefix(*event.Ref, headPrefix)
 				if found {
 					_, err = fmt.Fprintf(writer, "Push event created for branch: %v\n", branchname)
 					if err != nil {
@@ -104,4 +138,5 @@ func init() {
 	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle.")
 	rootCmd.Flags().BoolP("dry-run", "d", false, "Just generate execution resources without running them.")
 	rootCmd.Flags().IntP("port", "p", defaultPort, "Port for http server to listen to.")
+	rootCmd.Flags().StringP("config", "c", defaultPath, "Path to the main config file")
 }
--- git-proxy/go.mod
+++ git-proxy/go.mod
@@ -13,4 +13,5 @@ require (
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
 	go.uber.org/multierr v1.11.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
