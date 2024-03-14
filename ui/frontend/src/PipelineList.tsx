a2a11f5ee7a11a98a7ee7a950af99caf4f23c83f
David Lynch <davite3@protonmail.com>
2024-03-14 01:04:43 +0000

    UI TRAFFIC LIGHTS RENDERING FOR EXECUTIONS Rendering executions using traffic light model Green means success! Blue means running! Red means failing! :( Orange means pending!
    Will tidy react state because backend gets hammered by requests

--- ui/frontend/src/PipelineList.tsx
+++ ui/frontend/src/PipelineList.tsx
@@ -7,10 +7,11 @@ import "./index.css";
 export const PipelineList = (props: {
     namespace: string;
     focusedPipeline: number;
-    focusedExecution: number;
+    focusedExecution: string;
     pipelines: Array<Pipeline>;
     executions: Array<Execution>;
     setFocusedPipeline: any;
+    setFocusedExecution: any;
 }): React.ReactNode => (
     <div className="">
         {props.pipelines.length !== 0 && (
@@ -31,17 +32,28 @@ export const PipelineList = (props: {
                             }}
                         >
                             <p className="">{pipeline.metadata?.name}</p>
-                            <ul className="">
-                                {props.executions
-                                    .filter(
-                                        (exe: Execution) =>
-                                            exe.spec?.pipeline ==
-                                            pipeline.metadata?.name
-                                    )
-                                    .map((exe: Execution) => (
-                                        <li>{exe.metadata?.name}</li>
-                                    ))}
-                            </ul>
+                            {pipeline ==
+                                props.pipelines[props.focusedPipeline] && (
+                                <ul className="bg-slate-600">
+                                    {props.executions
+                                        .filter(
+                                            (exe: Execution) =>
+                                                exe.spec?.pipeline ==
+                                                pipeline.metadata?.name
+                                        )
+                                        .map((exe: Execution) => (
+                                            <li
+                                                onClick={() =>
+                                                    props.setFocusedExecution(
+                                                        exe.metadata?.name
+                                                    )
+                                                }
+                                            >
+                                                {exe.metadata?.name}
+                                            </li>
+                                        ))}
+                                </ul>
+                            )}
                         </li>
                     )
             )}
