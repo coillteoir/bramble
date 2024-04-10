0c4ad04eb5e7b65c66f38b61da5c7f7141f5924f
David Lynch <davite3@protonmail.com>
2024-04-10 18:25:11 +0100

    removed commented SVG animation, and added forest theme

--- ui/frontend/src/ExecutionGraph.tsx
+++ ui/frontend/src/ExecutionGraph.tsx
@@ -2,64 +2,6 @@ import { Node, Edge } from "reactflow";
 import { Pod } from "kubernetes-models/v1";
 import { pipelinesBrambleDev } from "./bramble-types";
 
-// https://www.benmvp.com/blog/how-to-create-circle-svg-gradient-loading-spinner/
-//const SVGPhase = () => (
-//    <svg
-//        width="20"
-//        height="20"
-//        viewBox="0 0 200 200"
-//        color="#3f51b5"
-//        fill="none"
-//        xmlns="http://www.w3.org/2000/svg"
-//        className="inline-block"
-//    >
-//        <defs>
-//            <linearGradient id="spinner-secondHalf">
-//                <stop offset="0%" stopOpacity="0" stopColor="currentColor" />
-//                <stop
-//                    offset="100%"
-//                    stopOpacity="0.5"
-//                    stopColor="currentColor"
-//                />
-//            </linearGradient>
-//            <linearGradient id="spinner-firstHalf">
-//                <stop offset="0%" stopOpacity="1" stopColor="currentColor" />
-//                <stop
-//                    offset="100%"
-//                    stopOpacity="0.5"
-//                    stopColor="currentColor"
-//                />
-//            </linearGradient>
-//        </defs>
-//
-//        <g strokeWidth="8">
-//            <path
-//                stroke="url(#spinner-secondHalf)"
-//                d="M 4 100 A 96 96 0 0 1 196 100"
-//            />
-//            <path
-//                stroke="url(#spinner-firstHalf)"
-//                d="M 196 100 A 96 96 0 0 1 4 100"
-//            />
-//
-//            <path
-//                stroke="currentColor"
-//                strokeLinecap="round"
-//                d="M 4 100 A 96 96 0 0 1 4 98"
-//            />
-//        </g>
-//
-//        <animateTransform
-//            from="0 0 0"
-//            to="360 0 0"
-//            attributeName="transform"
-//            type="rotate"
-//            repeatCount="indefinite"
-//            dur="1300ms"
-//        />
-//    </svg>
-//);
-
 export const generateNodes = (
     tasks: {
         name: string;
@@ -90,7 +32,7 @@ export const generateNodes = (
                             pod.metadata?.labels?.["bramble-execution"] &&
                         pod.metadata?.labels?.["bramble-task"] === task.name
                 );
-            const colour =
+            const spinner =
                 execution &&
                 (() => {
                     if (taskPod === undefined) {
@@ -150,7 +92,7 @@ export const generateNodes = (
                     label: (
                         <div>
                             <p>{task.name}</p>
-                            {colour}
+                            {spinner}
                         </div>
                     ),
                 },
